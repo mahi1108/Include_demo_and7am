30d4b5bcceb5ff220df8b9e8bca5a62076a6c649
maheshthippala <mahesh.cubex@gmail.com>
2018-12-26 08:18:47 +0530

    Initial commit

---
+++ app/src/main/java/cubex/mahesh/include_demo_and7am/MainActivity.kt
@@ -0,0 +1,12 @@
+package cubex.mahesh.include_demo_and7am
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
